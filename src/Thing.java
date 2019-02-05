f4e0a698a176c81785b6fd59f57e26ad7ce007ba
PeterJennings642 <47356999+PeterJennings642@users.noreply.github.com>
2019-02-05 09:55:00 -0500

    Update Thing.java

--- src/Thing.java
+++ src/Thing.java
@@ -2,6 +2,7 @@ public class Thing {
     public static void main(String[] args)
     {
         System.out.println("Hello, code!");
+        System.out.println("Hello, from Github");
         System.out.println("Hello, the third time");
     }
 }
